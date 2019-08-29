74bde14c8db4ac652d2e63cada606b9661f46b3b
sfktrkl <sfktrkl@gmail.com>
2019-08-30 01:16:22 +0300

    Create new test project
    * To test the interpreter quickly, test project is created and added 3 different tests for now (tests for shoot, input keywords and if statement)

---
+++ InterpreterTests/interpreterTests.cpp
@@ -0,0 +1,102 @@
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <direct.h>
+#include <ctime>
+#include "CppUnitTest.h"
+#include "../Interpreter/Interpreter.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace InterpreterTests
+{
+    TEST_CLASS(InterpreterTests)
+    {
+    private:
+        void saveFile(std::string fileContents)
+        {
+            _mkdir("..\\TestFiles");
+            std::ofstream myfile(filePath);
+            myfile << fileContents;
+            myfile.close();
+        }
+
+        const char* filePath;
+        std::string fileContents;
+        std::vector<int> inputs;
+        std::shared_ptr<Interpreter> interpreter = nullptr;
+
+        void Begin()
+        {
+            saveFile(fileContents);
+            interpreter = std::make_shared<Interpreter>(filePath);
+            interpreter->TakeInputs(inputs);
+            interpreter->Execute();
+        }
+    public:
+        TEST_METHOD(SHOOT)
+        {
+            filePath = "..\\TestFiles\\SHOOT.sfk";
+
+            std::ostringstream ss;
+            ss << "SHOOT 3 + 2";
+            fileContents = ss.str();
+
+            inputs = { };
+
+            Begin();
+
+            std::vector<int> outputs = interpreter->GiveOutputs();
+
+            Assert::AreEqual(size_t(1), outputs.size());
+            Assert::AreEqual(5, outputs[0]);
+        }
+
+        TEST_METHOD(INPUT)
+        {
+            filePath = "..\\TestFiles\\INPUT.sfk";
+
+            std::ostringstream ss;
+            ss  << "INPUT $MYVARIABLE\n"
+                << "SHOOT 3 + $MYVARIABLE";
+            fileContents = ss.str();
+
+            srand((int)time(0));
+            int randomNumber = rand();
+            inputs = { randomNumber };
+
+            Begin();
+
+            std::vector<int> outputs = interpreter->GiveOutputs();
+
+            Assert::AreEqual(size_t(1), outputs.size());
+            Assert::AreEqual(randomNumber + 3, outputs[0]);
+        }
+
+        TEST_METHOD(IF)
+        {
+            filePath = "..\\TestFiles\\IF.sfk";
+
+            std::ostringstream ss;
+            ss  << "INPUT $NUMBER\n"
+                << "IF $NUMBER % 2 == 0 THEN\n"
+                << "SHOOT 0\n"
+                << "ELSE\n"
+                << "SHOOT 1\n"
+                << "ENDIF";
+
+            fileContents = ss.str();
+
+            srand((int)time(0));
+            int randomNumber = rand() % 100;
+            inputs = { randomNumber };
+
+            Begin();
+
+            std::vector<int> outputs = interpreter->GiveOutputs();
+
+            Assert::AreEqual(size_t(1), outputs.size());
+            Assert::AreEqual(randomNumber % 2, outputs[0]);
+        }
+	};
+}
