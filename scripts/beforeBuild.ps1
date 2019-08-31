a573d4e9e30c1d928ce1bbc186941287e4c6be5a
sfktrkl <sfktrkl@gmail.com>
2019-08-31 15:52:04 +0300

    Extract trial 4

--- scripts/beforeBuild.ps1
+++ scripts/beforeBuild.ps1
@@ -2,7 +2,7 @@ Write-Host ""
 Write-Host "Extract externals."
 Write-Host "-----------------------------------------------------------------------"
 
-Resolve-Path ~
+Get-Location
 
 if (Test-Path externals\binaries) { Remove-Item externals\binaries -Recurse }
 if (Test-Path externals\include) { Remove-Item externals\include -Recurse }
