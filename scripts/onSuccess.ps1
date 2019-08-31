9d8efd20f3c389e9486d0dedabb826eb7a808cc6
sfktrkl <sfktrkl@gmail.com>
2019-08-31 13:46:20 +0300

    Artifacts trial 2

---
+++ scripts/onSuccess.ps1
@@ -0,0 +1,9 @@
+$files =
+(
+  "dependencies.txt",
+  "$Env:CONFIGURATION\InterpreterClassLibrary.dll",
+  "$Env:CONFIGURATION\OSGViewClassLibrary.lib",
+  "$Env:CONFIGURATION\Shoot++.exe"
+)
+
+7z a -snl shootpp.zip $files | Out-String
