0d63d07b35b3c9f24cd220ec2a45532eda7032f8
sfktrkl <sfktrkl@gmail.com>
2019-08-31 15:55:18 +0300

    Extract trial 6

--- scripts/beforeBuild.ps1
+++ scripts/beforeBuild.ps1
@@ -4,9 +4,8 @@ Write-Host "--------------------------------------------------------------------
 
 Test-Path externals\binaries
 Test-Path externals\include
-Test-Path externals\lib
-
-if (Test-Path externals\binaries) { Remove-Item externals\binaries -Recurse }
-if (Test-Path externals\include) { Remove-Item externals\include -Recurse }
-if (Test-Path externals\lib) { Remove-Item externals\lib -Recurse }
-7z e -snl externals\externals.7z
+Test-Path sfktrkl
+Test-Path shootpp
+Test-Path sfktrkl\shootpp
+Test-Path sfktrkl\shootpp\externals
+Test-Path sfktrkl\shootpp\binaries
