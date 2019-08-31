6441beee66cf81833b74f4b05946be9097c28f8a
sfktrkl <sfktrkl@gmail.com>
2019-08-31 15:17:02 +0300

    Artifacts trial 14

--- scripts/afterTest.ps1
+++ scripts/afterTest.ps1
@@ -3,12 +3,12 @@ Write-Host "Zipping artifacts."
 Write-Host "-----------------------------------------------------------------------"
 
 
-$files =
-(
-  ".\externals\binaries\*.dll",
-  ".\$Env:CONFIGURATION\InterpreterClassLibrary.dll",
-  ".\$Env:CONFIGURATION\OSGViewClassLibrary.dll",
-  ".\$Env:CONFIGURATION\Shoot++.exe"
-)
+if (Test-Path Shoot++) { Remove-Item Shoot++ -Recurse }
+if (Test-Path Shoot++.zip) { Remove-Item Shoot++.zip -Recurse }
+New-Item -Name Shoot++ -ItemType "directory"
+Copy-Item -Path ..\externals\binaries\*.dll -Destination Shoot++
+Copy-Item -Path ..\Release\InterpreterClassLibrary.dll -Destination Shoot++
+Copy-Item -Path ..\Release\OSGViewClassLibrary.dll -Destination Shoot++
+Copy-Item -Path ..\Release\Shoot++.exe -Destination Shoot++
 
-7z a -snl shootpp.zip $files Shoot++\
+7z a -snl Shoot++.zip $files Shoot++
