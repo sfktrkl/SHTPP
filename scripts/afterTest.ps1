d2c8998a82edddcd37343fc192e8e5c06337986a
sfktrkl <sfktrkl@gmail.com>
2019-08-31 15:19:41 +0300

    Artifacts trial 15

--- scripts/afterTest.ps1
+++ scripts/afterTest.ps1
@@ -6,9 +6,9 @@ Write-Host "--------------------------------------------------------------------
 if (Test-Path Shoot++) { Remove-Item Shoot++ -Recurse }
 if (Test-Path Shoot++.zip) { Remove-Item Shoot++.zip -Recurse }
 New-Item -Name Shoot++ -ItemType "directory"
-Copy-Item -Path ..\externals\binaries\*.dll -Destination Shoot++
-Copy-Item -Path ..\Release\InterpreterClassLibrary.dll -Destination Shoot++
-Copy-Item -Path ..\Release\OSGViewClassLibrary.dll -Destination Shoot++
-Copy-Item -Path ..\Release\Shoot++.exe -Destination Shoot++
+Copy-Item -Path externals\binaries\*.dll -Destination Shoot++
+Copy-Item -Path Release\InterpreterClassLibrary.dll -Destination Shoot++
+Copy-Item -Path Release\OSGViewClassLibrary.dll -Destination Shoot++
+Copy-Item -Path Release\Shoot++.exe -Destination Shoot++
 
 7z a -snl Shoot++.zip $files Shoot++
