d635eed360cf044914044aa291bc85bd5d745b24
sfktrkl <sfktrkl@gmail.com>
2019-08-31 14:26:37 +0300

    Artifacts trial 9

--- scripts/afterTest.ps1
+++ scripts/afterTest.ps1
@@ -5,10 +5,10 @@ Write-Host "--------------------------------------------------------------------
 
 $files =
 (
-  "$Env:APPVEYOR_REPO_NAME\.\externals\binaries\*.dll",
-  "$Env:APPVEYOR_REPO_NAME\InterpreterClassLibrary.dll",
-  "$Env:APPVEYOR_REPO_NAME\OSGViewClassLibrary.dll",
-  "$Env:APPVEYOR_REPO_NAME\Shoot++.exe"
+  ".\externals\binaries\*.dll",
+  "$Env:CONFIGURATION\InterpreterClassLibrary.dll",
+  "$Env:CONFIGURATION\OSGViewClassLibrary.dll",
+  "$Env:CONFIGURATION\Shoot++.exe"
 )
 
 7z a -snl shootpp.zip $files | Out-String
